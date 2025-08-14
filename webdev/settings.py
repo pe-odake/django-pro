bc59d3071d6d5150d2d52f478891937c6d54c7d3
Pedro Odake <pedroodake@gmail.com>
2025-08-14 16:21:38 -0300

    Lista de contatos com django feita

--- webdev/settings.py
+++ webdev/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5ah3trzu*lql^&ztorah@97kv0bd5^4*c-*41w6*rdrn$95u8o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pedroodake.pythonanywhere.com']
+ALLOWED_HOSTS = ['pedroodake.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
