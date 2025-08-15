d7c7c18610200597a0fa9c634e476420670b23dc
Pedro Odake <pedroodake@gmail.com>
2025-08-15 13:56:59 -0300

    tentativa com asterisco2

--- webdev/settings.py
+++ webdev/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5ah3trzu*lql^&ztorah@97kv0bd5^4*c-*41w6*rdrn$95u8o
 DEBUG = True
 
 # ALLOWED_HOSTS = ['pedroodake.pythonanywhere.com', '127.0.0.1']
- ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
